eefe363937be1a571ba4cdb68b189b905078c888
Raúl López <hi@raullg.com>
2023-05-12 18:00:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/src/Logger.php';
+require_once __DIR__ . '/src/loggers/ConsoleLogger.php';
+
+$logger = new Logger();
+$logger->addLogger(new ConsoleLogger(LogLevel::WARNING));
+
+$logger->debug('Debug message');
+$logger->info('Info message');
+$logger->warning('Warning message');
+$logger->error('ERROR message');
